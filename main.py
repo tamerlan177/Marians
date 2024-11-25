ec6a61b0f5065001f998716c27c2511db0126f4d
tamerlan177 <tama06olimp@gmail.com>
2024-11-25 17:22:32 +0600

    main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import random
 
 location = [2, 3, 4];
 weight = [200, 250, 263]
+
+
+def verification_weight():
+    global weight
+    global location
+
+    if sum(weight) == 713:
+        print('You found cargo')
+    else:
+        print("It does not have needed weight")
+      
+        location = random.sample(range(8), 3)
+
